a21f33861b008b7f56ea8c86618a63fd0f1e80ec
dedecanton <andrecanton18@gmail.com>
2022-03-07 08:12:11 -0300

    aditional config ts added

---
+++ src/custom.d.ts
@@ -0,0 +1,14 @@
+declare module "*.module.css";
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module '*.svg?inline' {
+    const content: any
+    export default content
+  }
+  
+  declare module '*.svg' {
+    const content: any
+    export default content
+  }
