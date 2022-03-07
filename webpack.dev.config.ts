486b373ba4326e7b7fbbad2f0389d75d32c413e4
dedecanton <andrecanton18@gmail.com>
2022-03-07 08:09:50 -0300

    setup config

---
+++ webpack.dev.config.ts
@@ -0,0 +1,75 @@
+import path from "path";
+import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+
+const config: Configuration = {
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  entry: ["regenerator-runtime/runtime.js", "./src/index.tsx"],
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new HotModuleReplacementPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new ESLintPlugin({
+      extensions: ["js", "jsx", "ts", "tsx"],
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.join(__dirname, "build"),
+    historyApiFallback: true,
+    port: 4000,
+    open: true,
+    hot: true,
+  },
+};
+
+export default config;
