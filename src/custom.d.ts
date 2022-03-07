cdd10999983c3cba4363aa2a13e986794e463174
dedecanton <andrecanton18@gmail.com>
2022-03-07 08:41:40 -0300

    svg config

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -5,12 +5,7 @@ declare module "*.jpg";
 declare module "*.png";
 declare module "*.jpeg";
 declare module "*.gif";
-declare module '*.svg?inline' {
-    const content: any
-    export default content
-  }
-  
-  declare module '*.svg' {
-    const content: any
-    export default content
-  }
+declare module "*.svg" {
+  const svg: string;
+  export default svg;
+}
