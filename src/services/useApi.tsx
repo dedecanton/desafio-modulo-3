f9584ed96fe97e0433eb8a7f13bdae9e27171d16
dedecanton <andrecanton18@gmail.com>
2022-03-08 17:22:19 -0300

    custom hook

--- src/services/useApi.tsx
+++ src/services/useApi.tsx
@@ -1,30 +1,46 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-import { useState, useEffect } from 'react';
+import { useCallback, useEffect, useState } from "react"
+
+const URL = 'https://62224954666291106a22d82f.mockapi.io/todo'
 
 
-const BASE_URL = 'https://62224954666291106a22d82f.mockapi.io/todo'
+export default function useApi ():any{
 
-const useApi = ():any => {
-    const [loading, setLoading] = useState(true)
-    const [data, setData] = useState(null)
-  
-    const fetchApi = () => {
-      fetch(BASE_URL) 
-      .then(response => {
-        return response.json()
-      })
-      .then(json => {
-        console.log(json)
-        setLoading(false)
-        setData(json)
-      })
-    };
-  
-    useEffect(() => {
-      fetchApi();
-    }, []);
-  
-    return { loading, data }
-  };
-  
-  export default useApi;
+   const [data, setData] = useState<any[]>([])
+   const [isLoading, setIsLoading] = useState(false);
+   
+
+   const fetchData = useCallback(async () => {
+     setIsLoading(true)
+
+     try{
+       const response = await fetch(URL)
+       if(!response.ok){
+         throw new Error('Something went wrong!')
+       }
+      const data = await response.json() 
+
+      const loadedTasks:any = []
+
+      data.forEach((data:any) => loadedTasks.push(data))
+      setData(loadedTasks)
+     }catch(err){
+       console.log(err)
+     }
+     setIsLoading(false)
+   },[])
+
+   useEffect(() => {
+     fetchData()
+   },[fetchData])
+
+
+   const deleteData = (id:string) => {
+     fetch(`https://62224954666291106a22d82f.mockapi.io/todo/${id}`, {method:'DELETE'})
+   }
+
+   
+
+    return { data, loading: isLoading, deleteData, fetchData }
+
+}
