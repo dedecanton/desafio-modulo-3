5df3a8b1a17c0fbc98112c8b5e63c415a9a245e9
dedecanton <andrecanton18@gmail.com>
2022-03-09 17:47:40 -0300

    auth tests

--- src/tests/Auth/Auth.spec.tsx
+++ src/tests/Auth/Auth.spec.tsx
@@ -1,13 +1,46 @@
-import {  render } from "@testing-library/react";
-import * as React from 'react';
-import AuthContainer from "../../container/AuthContainer";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import * as React from "react";
+import InputArea from "../../pages/Auth/InputArea";
 
-describe('Auth tests', () => {
+const onLogin = jest.fn();
+const onChangeEmail = jest.fn();
+const onChangePassword = jest.fn();
 
-    
-    test('text "Que bom que você voltou" should be visible!', () => {
-        render(<AuthContainer />)
+describe("Auth tests", () => {
+
+    beforeEach(() => {
+        render(
+            <InputArea
+              onLogin={onLogin}
+              onChangeEmail={onChangeEmail}
+              onChangePassword={onChangePassword}
+            />
+          );
     })
 
+  test('text "Que bom que você voltou" should be visible!', () => {
+    expect(screen.getByText("Que bom que você voltou!")).toBeVisible()
+  });
 
+  test('on change email input, should call onChangeEmail', () => {
+      const emailInput = screen.getByLabelText('Email')
+      userEvent.type(emailInput, 'teste')
+
+      expect(onChangeEmail).toBeCalledTimes(5)
+  })
+
+  test('on change password input, should call onChangeEmail', () => {
+      const passwordInput = screen.getByLabelText('Senha')
+      userEvent.type(passwordInput, 'teste')
+
+      expect(onChangePassword).toBeCalledTimes(5)
+  })
+
+  test('on button click, should call onLogin', () => {
+    const button = screen.getByText('Entrar')
+    userEvent.click(button)
+    expect(onLogin).toBeCalledTimes(1)
 })
+
+});
